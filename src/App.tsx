0a96abb80c031194ddefe58903fdf82cf35b7498
Gustavo <u201914306@upc.edu.pe>
2024-02-14 12:59:48 -0500

    primer commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,13 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
+      "Estas segure?",
+      "porfe",
+      "Segura?",
+      "te inivto un helado",
+      "un chocolate?",
+      "te regalo un poste",
+      "pls",
       "Estoy muerto",
       "No :(",
     ];
@@ -38,7 +32,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold"> siii, Te quiero mucho!!;))</div>
         </>
       ) : (
         <>
@@ -46,14 +40,14 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Quieres ser mi San Valentín?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              Si
             </button>
             <button
               onClick={handleNoClick}
